11add2d318e7f4429812879aef53f2e53b1556b6
MarkoDanilovic02 <mdanilovic06789@gmail.com>
2024-04-22 12:25:03 +0200

    initial chat components

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -17,6 +17,34 @@ const ChatList = () => {
           onClick={() => setAddMode((prev) => !prev)}
         />
       </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="text">
+          <span>Luka</span>
+          <p>Hello World!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="text">
+          <span>Luka</span>
+          <p>Hello World!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="text">
+          <span>Luka</span>
+          <p>Hello World!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="text">
+          <span>Luka</span>
+          <p>Hello World!</p>
+        </div>
+      </div>
     </div>
   );
 };
