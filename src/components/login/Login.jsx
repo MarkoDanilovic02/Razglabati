b10f5d238d14854f316650b3a790c7d27646b704
MarkoDanilovic02 <mdanilovic06789@gmail.com>
2024-04-25 21:01:25 +0200

    sign up with e-mail and address

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,6 @@
 import "./login.css";
 import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
   const [avatar, setAvatar] = useState({
@@ -16,6 +17,17 @@ const Login = () => {
     }
   };
 
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { username, email, password } = Object.fromEntries(formData);
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="login">
       <div className="item">
@@ -29,7 +41,7 @@ const Login = () => {
       <div className="separator"></div>
       <div className="item">
         <h2>Create an account</h2>
-        <form>
+        <form onSubmit={handleRegister}>
           <label htmlFor="file">
             <img src={avatar.url || "./avatar.png"} alt="" />
             Upload an image
