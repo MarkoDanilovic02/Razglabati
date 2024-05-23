9b0ecebb05493040196ae12c104c46eaaa888334
MarkoDanilovic02 <mdanilovic06789@gmail.com>
2024-05-23 19:55:39 +0200

    Logout on more

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,8 +1,24 @@
 import "./userInfo.css";
 import { useUserStore } from "../../../lib/userStore";
+import { useState } from "react";
+import { auth } from "../../../lib/firebase";
+import { useChatStore } from "../../../lib/chatStore";
 
 const UserInfo = () => {
   const { currentUser } = useUserStore();
+  const [showMore, setShowMore] = useState(false);
+
+  const { resetChat } = useChatStore();
+
+  const handleMore = () => {
+    setShowMore((prevState) => !prevState);
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
+    resetChat();
+  };
+
   return (
     <div className="userInfo">
       <div className="user">
@@ -10,10 +26,17 @@ const UserInfo = () => {
         <h2>{currentUser.username}</h2>
       </div>
       <div className="icons">
-        <img src="./more.png" alt="" />
+        <img src="./more.png" alt="" onClick={handleMore} />
         <img src="./video.png" alt="" />
         <img src="./edit.png" alt="" />
       </div>
+      {showMore && (
+        <div className="logoutBtn">
+          <button className="logout" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 };
